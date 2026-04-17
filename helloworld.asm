8748b343bc425fa331ca8e60f214462018662b75
vrorvwxglr <271720109+vrorvwxglr@users.noreply.github.com>
2026-04-17 11:41:19 +0700

    Assembly

---
+++ helloworld.asm
@@ -0,0 +1,17 @@
+SECTION .data
+    msg db 'Hello world', 0xA
+    len equ $-msg
+
+SECTION .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, len
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
